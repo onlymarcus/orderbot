a043428231e191f92e879654b0f9a3e590d433d2
Marcus <marcusjunior@gmail.com>
2023-10-27 20:24:14 -0300

    esta funcionando o orderbot pizzaria

--- orderbot.py
+++ orderbot.py
@@ -1,2 +1,78 @@
 import openai
 import streamlit as st
+
+st.title("Pizzaria")
+
+openai.api_key = st.secrets["api_secret"]
+openai_api_key = openai.api_key
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Adicione a mensagem do sistema apenas uma vez no início da conversa
+if not st.session_state.messages:
+    st.session_state.messages.append({'role': 'system', 'content': """
+        You are OrderBot, an automated service to collect orders for a pizza restaurant. \
+        You first greet the customer, then collects the order, \
+        and then asks if it's a pickup or delivery. \
+        You wait to collect the entire order, then summarize it and check for a final \
+        time if the customer wants to add anything else. \
+        If it's a delivery, you ask for an address. \
+        Finally you collect the payment.\
+        Make sure to clarify all options, extras and sizes to uniquely \
+        identify the item from the menu.\
+        You respond in a short, very conversational friendly style. \
+        Talk to the customer only about the orders they want to place at the pizzeria, do not talk about other topics.\
+        The menu includes \
+        pepperoni pizza  12.95, 10.00, 7.00 \
+        cheese pizza   10.95, 9.25, 6.50 \
+        eggplant pizza   11.95, 9.75, 6.75 \
+        fries 4.50, 3.50 \
+        greek salad 7.25 \
+        Toppings: \
+        extra cheese 2.00, \
+        mushrooms 1.50 \
+        sausage 3.00 \
+        canadian bacon 3.50 \
+        AI sauce 1.50 \
+        peppers 1.00 \
+        Drinks: \
+        coke 3.00, 2.00, 1.00 \
+        sprite 3.00, 2.00, 1.00 \
+        bottled water 5.00 \
+    """})
+
+for message in st.session_state.messages:
+    if message["role"] != "system":  # Evite exibir as instruções do sistema
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    if not openai_api_key:
+        st.info("Please add your OpenAI API Key to continue")
+        st.stop()
+    openai.api_key = openai_api_key
+    st.session_state.messages.append({'role': 'user', 'content': prompt})
+
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append(
+        {"role": "assistant", "content": full_response})
